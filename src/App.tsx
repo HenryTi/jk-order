6988f3345f84a6c033634ed282418962abfc0df6
Henry <liaohengyi@outlook.com>
2018-09-19 11:12:16 -0400

    Cr=>C Vm=>V

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import {CrApp} from 'tonva-react-usql';
+import {CApp} from 'tonva-react-usql';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 // import logo from './logo.svg';
 import { NavView } from 'tonva-tools';
 
-const tonvaApp = 'jk-dev/jkOrder';
+const tonvaApp = 'JKDev/jkOrder';
 let ui:any = undefined;
 
 class App extends React.Component {
     async onLogined() {
-      let crApp = new CrApp(tonvaApp, ui);
-      await crApp.start();
+      let cApp = new CApp(tonvaApp, ui);
+      await cApp.start();
     }
     render() {
         return <NavView onLogined={this.onLogined} />;
